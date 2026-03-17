b3d421baf171fb3ff150afde7ccafd324346ecf9
jkdev91 <devjk91@outlook.com>
2026-03-17 13:02:57 +0100

    refactor: nettoyage converters.js, suppression fonctions inutiles

--- services/converters.js
+++ services/converters.js
@@ -1,14 +1,3 @@
-export const ctoF = (c) => (c * 9) / 5 + 32;
-
-export const mpsToMph = (mps) => (mps * 2.236936).toFixed(2);
-
-export const kmToMiles = (km) => (km / 1.609).toFixed(1);
-
-export const timeTo12HourFormat = (time) => {
-  let [hours, minutes] = time.split(":");
-  return `${(hours %= 12) ? hours : 12}:${minutes}`;
-};
-
 export const degToCompass = (num) => {
   var val = Math.round(num / 22.5);
   var arr = [
@@ -30,12 +19,4 @@ export const degToCompass = (num) => {
     "NNW",
   ];
   return arr[val % 16];
-};
-
-export const unixToLocalTime = (unixSeconds, timezone) => {
-  let time = new Date((unixSeconds + timezone) * 1000)
-    .toISOString()
-    .match(/(\d{2}:\d{2})/)[0];
-
-  return time.startsWith("0") ? time.substring(1) : time;
 };
